40e495033a23d7eadd5b2f3790849c20056a052d
JoshuaSitumeang <160564706+JoshuaSitumeang@users.noreply.github.com>
2024-03-24 08:45:16 +0000

    Assignment 5b Trial Submission

--- main.py
+++ main.py
@@ -25,3 +25,14 @@ def write_csv(filename, header, data):
 # TESTING
 # You can write code below to call the above functions
 # and test the output
+def read_csv("pre-u-enrolment-by-age.csv"):
+    open('pre-u-enrolment-by-age.csv', 'r')
+file_handle = open('pre-u-enrolment-by-age.csv', 'r')
+print(f'type {type(file_handle)}')
+enrolment_by_age: [enrolment_jc]
+sex: [MF, F]
+age: [int]
+year: [int]
+sum_by_year(enrolment_data): [[year, total_enrolment]]
+write_csv(pre-u-enrolment-by-age.csv, header, enrolment_by_year)
+    
