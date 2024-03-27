21f0e7edd5777522bebcf38fa95f6032ee0a85b3
JoshuaSitumeang <160564706+JoshuaSitumeang@users.noreply.github.com>
2024-03-27 06:36:15 +0000

    Assignment 5b

--- main.py
+++ main.py
@@ -1,38 +1,122 @@
 # Part 1
-def read_csv(filename):
     # Type your code below
-    pass
+def read_csv(filename):
+    """
+    takes in a filename and processes its contents
+    
+    Parameter
+    ----------
+    filename: str
+      name of a file
+    
+    Returns
+    ---------
+    tuple
+      tuple in the format (header, data)
+    """
+    data = []
+    with open(filename, 'r') as f:
+        header = f.readline().strip()
+        for line in f:
+            record = line.strip().split(",")
+            record[0] = int(record[0])
+            record[3] = int(record[3])
+            data.append(record)
+
+       #f.close() is called automatically
+
+    return header, data
+
+read_csv('pre-u-enrolment-by-age.csv')
 
 
 # Part 2
 def filter_gender(enrolment_by_age, sex):
+    """
+    filter data according to enrolment_by_age and sex
+
+    Parameter
+    ----------
+    filename: str
+        name of a file
+
+    Returns
+    ---------
+    tuple
+        tuple in the format [year, age, enrolment_jc]
+    """
     # Type your code below
-    pass
+    filtered_data = []
+    for rec in enrolment_by_age:
+        if rec[2] == sex:
+            filtered_data.append([rec[0], rec[1], rec[3]])
+
+    return filtered_data
 
 
 # Part 3
 def sum_by_year(enrolment):
+    """
+    adds up total enrolment for each year, regardless of age
+
+    Parameter
+    ---------
+    filename: int
+        name of a file
+
+    Returns
+    ---------
+    tuple
+        tuple in the format [year, total_enrolment]
+    """
     # Type your code below
-    pass
+    year_list = []
+    sum_list = []
+    sum = 0
+    year_list.append(enrolment_data[0][0])
+    for rec in enrolment_data:
+        year = rec[0]
+        if year not in year_list:
+            year_list.append(year)
+            sum_list.append(sum)
+            sum = 0
+
+        sum += rec[2]
+
+    sum_list.append(sum)
+
+    enrolment_by_year = []
+    for i in range(len(year_list)):
+        enrolment_by_year.append([year_list[i], sum_list[i]])
+
+    return enrolment_by_year
 
 
 # Part 4
 def write_csv(filename, header, data):
+    """
+    Convert data to CSV format and return the number of lines written
+
+    Parameter
+    ---------
+    filename: csv
+        name of a file
+
+    Returns
+    file 
+        file in the csv format
+    """
     # Type your code below
-    pass
+    with open(filename, 'w') as f:
+        output = f'{rec[0]},{rec[1]}\n'
+        f.write(output)
+
+        count += 1
+
 
 
 # TESTING
 # You can write code below to call the above functions
 # and test the output
-def read_csv("pre-u-enrolment-by-age.csv"):
-    open('pre-u-enrolment-by-age.csv', 'r')
-file_handle = open('pre-u-enrolment-by-age.csv', 'r')
-print(f'type {type(file_handle)}')
-enrolment_by_age: [enrolment_jc]
-sex: [MF, F]
-age: [int]
-year: [int]
-sum_by_year(enrolment_data): [[year, total_enrolment]]
-write_csv(pre-u-enrolment-by-age.csv, header, enrolment_by_year)
-    
+
+print(read_csv('pre-u-enrolment-by-age.csv'))
